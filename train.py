54eb34c19dce27d77631651b20a903be101bd30e
hao <2200010929@stu.pku.edu.cn>
2025-04-25 01:22:40 +0800

    [2200010929] support generation/sampling and device auto-selecting

--- train.py
+++ train.py
@@ -140,4 +140,45 @@ class GPT(nn.Module):
 
         return model
 
-model = GPT.from_pretrained('gpt2')
+
+num_return_sequences = 5
+max_length = 30
+
+device = "cpu"
+if torch.cuda.is_available():
+    device = "cuda"
+
+print(f"using device: {device}")
+
+model = GPT(GPTConfig())
+
+model.eval()
+model.to(device)
+
+
+import tiktoken
+enc = tiktoken.get_encoding('gpt2')
+tokens = enc.encode("Hello, I'm a language model,")
+tokens = torch.tensor(tokens, dtype=torch.long) 
+tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1) 
+x = tokens.to(device)
+
+
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+while x.size(1) < max_length:
+    with torch.no_grad():
+        logits = model(x) 
+        logits = logits[:, -1, :] # (B, vocab_size)
+        probs = F.softmax(logits, dim=-1)
+        # do top-k sampling of 50 (huggingface pipeline default)
+        # topk_probs here becomes (5, 50), topk_indices is (5, 50)
+        topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)
+        ix = torch.multinomial(topk_probs, 1) 
+        xcol = torch.gather(topk_indices, -1, ix) 
+        x = torch.cat((x, xcol), dim=1)
+
+for i in range(num_return_sequences):
+    tokens = x[i, :max_length].tolist()
+    decoded = enc.decode(tokens)
+    print(">", decoded)
