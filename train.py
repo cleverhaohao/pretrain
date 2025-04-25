ad99268a86badb0506a1336d36cc6d8864854e72
hao <2200010929@stu.pku.edu.cn>
2025-04-25 17:49:45 +0800

    [2200010929] add a very simple optimizer

--- train.py
+++ train.py
@@ -162,15 +162,22 @@ text = text[:1000]
 tokens = enc.encode(text)
 B, T = 4, 32
 buf = torch.tensor(tokens[:B*T + 1])
+buf = buf.to(device)
 x = buf[:-1].view(B, T)
 y = buf[1:].view(B, T)
 
 model = GPT(GPTConfig())
 model.to(device)
-logits, loss = model(x,y)
 
-print(logits.shape)
-print(loss)
+optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+for i in range(50):
+    optimizer.zero_grad()
+    logits, loss = model(x, y)
+    loss.backward()
+    optimizer.step()
+    print(f"step {i}, loss: {loss.item()}")
+
+
 import sys; sys.exit(0)
 
 
